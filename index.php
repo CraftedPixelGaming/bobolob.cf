6b7d1f5335bd1c989179db596c78e9438d55ae05
Minion3665 <pixtopia@null.net>
2018-10-16 08:50:32 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,83 @@
+<head>
+<style>
+@keyframes flyin {
+    from {opacity: 0.2; top: 12vw;}
+    to {opacity: 1; top: 4vw;}
+}
+.startinganimation {
+    position: fixed;
+    top: 4vw;
+    animation-name: flyin;
+    animation-duration: 1s;
+    left: 0;
+}
+a {
+    display: block;
+    height: 20vw;
+    width: 20vw;
+    word-wrap: break-word;
+    text-decoration: none;
+    color: #aaaaaa;
+    font-size: 3vw;
+    text-align: center;
+
+}
+a:hover, a:active {
+    display: block;
+    height: 20vw;
+    width: 20vw;
+    word-wrap: break-word;
+    text-decoration: none;
+    color: white;
+    font-size: 3vw;
+    text-align: center;
+}
+.text {
+    margin-top: 50%;
+    transform: translate(0, -50%);
+}
+.title {
+    display: block;
+    position: fixed;
+    text-align: center;
+    padding-left: 3vw;
+    transform: translate(0px, 24vw);
+    font-size: 20vw;
+    color: white;
+}
+body {
+    background-color: #330000;
+    position: fixed;
+}
+.robloxDiv {
+    background-color: #3333aa;
+    position: fixed;
+    left: 4vw;
+}
+.youtubeDiv {
+    background-color: #aa0000;
+    position: fixed;
+    left: 28vw;
+}
+.discordDiv {
+    background-color: #2C2F33;
+    position: fixed;
+    left: 52vw;
+}
+.emailDiv {
+    background-color: #555555;
+    position: fixed;
+    left: 76vw;
+}
+</style>
+<title>BobolobYT</title>
+</head>
+<body>
+<div class="startinganimation">
+    <a href="https://www.roblox.com/users/284283474/profile" class="robloxDiv"><div class="text">Bobolob on Roblox</div></a>
+    <a href="https://youtube.com/channel/UC4345ENswOQf86Bit-DNPsQ" class="youtubeDiv"><div class="text">Bobolob on YouTube</div></a>
+    <a href="https://discord.gg/NHzddvP" class="discordDiv"><div class="text">Bobolob on Discord</div></a>
+    <a href="javascript:void(0);" class="emailDiv"><div class="text">Email:<br>YTsupport<wbr>@bobolob.cf</div></a>
+    <div class="title">BOBOLOB</div>
+</div>
+</body>
